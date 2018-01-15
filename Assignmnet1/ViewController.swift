5db3d31d9e4510f5e5a5e2f7c37b9e59de947608
Student <student@CVS-R213D013.nic.bc.ca>
2018-01-15 12:36:03 -0800

    First part completed

--- Assignmnet1/ViewController.swift
+++ Assignmnet1/ViewController.swift
@@ -9,7 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var signOperator: UILabel!
+    
 
+    @IBOutlet weak var firstOperand: UILabel!
+    
+    
+    @IBOutlet weak var secondOperand: UILabel!
+    
+    @IBOutlet weak var result: UITextField!
+    
+    
+    @IBOutlet weak var score: UILabel!
+    @IBOutlet weak var level: UILabel!
+    
+    @IBOutlet weak var round: UILabel!
+    
+    
+    
+    var first_Operand_Number: Int = 0
+    var second_Operand_Number: Int = 0
+    var number: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +42,57 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+   
+    @IBAction func resultCheck(_ sender: UIButton) {
+        
+        randomoperators()
+        randomOperands()
+    }
+    
+   
+    @IBAction func startOverbtn(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func infobtn(_ sender: UIButton) {
+    }
+    
+    func randomoperators(){
+    number = Int(arc4random_uniform(4))
+        switch(number){
+            
+        case 0: signOperator.text = "+"
+            break
+        case 1: signOperator.text = "-"
+            break
+        case 2: signOperator.text = "*"
+            break
+        case 3: signOperator.text = "/"
+            break
+        default: signOperator.text = "+"
+            break
+        }
+        
+    }
+    
+    func randomOperands(){
+        
+       first_Operand_Number = Int(arc4random_uniform(10))
+       firstOperand.text = String(first_Operand_Number)
+       second_Operand_Number = Int(arc4random_uniform(10))
+       secondOperand.text = String(second_Operand_Number)
+        
+    }
+    
+    func answerCheck(){
+    
+        
+        
+    }
 
-
+    
 }
+
+
 
